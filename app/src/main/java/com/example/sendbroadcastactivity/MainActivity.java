8a8a76bb1ef53e4048cec3d6075de291fbd5b97d
taochangbang123 <56054424+taochangbang123@users.noreply.github.com>
2020-07-22 13:30:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sendbroadcastactivity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sendbroadcastactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.content.BroadcastReceiver;
+
+public class MainActivity extends AppCompatActivity {
+    BroadcastReceiver receiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        configureReceiver();
+    }
+    public void broadcastIntent(View view)
+    {
+        Intent intent = new Intent();
+        intent.setAction("com.ngocminhtran.sendbroadcast");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+    }
+    private void configureReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.ngocminhtran.sendbroadcast");
+        filter.addAction("android.intent.action.ACTION_POWER_DISCONNECTED");
+        receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+}
